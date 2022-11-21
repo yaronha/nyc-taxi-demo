d10284778fb58983abfe7f49d80a5365bead6412
davesh0812 <davesh0812@gmail.com>
2022-11-21 11:44:04 +0200

    lint tests

--- tests/test_data_prep.py
+++ tests/test_data_prep.py
@@ -1,5 +1,3 @@
-import pandas as pd
-
 from src.data_prep import *
 
 
@@ -7,7 +5,7 @@ def test_data_preparation_pipline():
     df = get_data()
     train, test, label = data_preparation(df, 0.2)
 
-    assert label == 'fare_amount'
+    assert label == "fare_amount"
 
     # check for expected types
     assert isinstance(train, pd.DataFrame)
@@ -19,9 +17,9 @@ def test_clean_df():
     df = clean_df(df)
 
     # check if df contains unexpected values
-    assert 0 in df['fare_amount']
-    assert 0 in df['pickup_longitude']
+    assert 0 in df["fare_amount"]
+    assert 0 in df["pickup_longitude"]
 
 
 def get_data():
-    return pd.read_csv('../data/dataset.csv')
+    return pd.read_csv("../data/dataset.csv")
